37f29ebf5e7692de8f24d0f024df6b61e06d92a6
GeonwooVincentKim <kdsnop@gmail.com>
2023-07-10 15:02:01 +0900

    [Feature] - Testing with Maven Project

---
+++ src/main/java/com/example/maven_first_project/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_first_project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
